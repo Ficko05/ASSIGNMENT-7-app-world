5c38ab917e266d8b70713712c95e26355dbd4940
Ficko <you@example.com>
2019-03-29 13:46:39 +0100

    monster and gold

---
+++ Assets/monsterScripts/mogHit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mogHit : MonoBehaviour {
+
+    public int coins;
+    public Text countText;
+
+    public void onclick()
+    {
+        print("Sprite Clicked");
+
+        coins++;
+        setCountText();
+    }
+
+   
+
+    public void setCountText()
+    {
+        countText.text = "Gold x " + coins.ToString();
+    }
+}

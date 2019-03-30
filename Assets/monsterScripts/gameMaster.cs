5930af7644d687bc7ec303397f1441bc2f38f60e
Ficko <filip-f@hotmail.com>
2019-03-30 18:27:26 +0100

    gold stuff, home pc

---
+++ Assets/monsterScripts/gameMaster.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameMaster : MonoBehaviour {
+
+    public float coins ;
+    public Text countText;
+    public float goldMultiplyser = 1;
+
+    public void onclickMonster()
+    {
+
+        goldBank(goldMultiplyser);
+        setCountText();
+    }
+
+
+    public void goldBank(float x)
+    {
+        coins = coins + x;
+        
+    }
+
+    public void goldUpgrade()
+    {
+        if(coins >= 10)
+        {
+            print("buy");
+            coins = coins - 10;
+            setCountText();
+            goldMultiplyser = goldMultiplyser * 2;
+        }
+        print("pressed");
+
+    }
+
+    void setCountText()
+    {
+        countText.text = "Gold x " + coins.ToString();
+    }
+}
